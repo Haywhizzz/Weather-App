2516370a1df82ceaebf50f0a62d6f43e045ec2fe
haywhizzz <haywhizzz@gmail.com>
2021-02-19 14:37:40 +0100

    Fix eslint errors

--- src/logicModule.js
+++ src/logicModule.js
@@ -1,7 +1,7 @@
 export default (formId) => {
-    const form = document.getElementById(formId);
-    if (form.elements.namedItem('city').value) {
-        return form.elements.namedItem('city').value;
-    }
-    return null;
+  const form = document.getElementById(formId);
+  if (form.elements.namedItem('city').value) {
+    return form.elements.namedItem('city').value;
+  }
+  return null;
 };
